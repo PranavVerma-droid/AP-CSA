723924fe66bd9ea9f769dc102a4173acc7d0f87f
Pranav Verma <pranav@verma.net.in>
2025-01-08 01:50:01 +0530

    Added Section 1.1

---
+++ Unit 1/Section 1.1/Print.java
@@ -0,0 +1,10 @@
+/*
+ * Written by Pranav Verma
+ * For the AP CSA Course.
+ */
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Unit 1/Section 1.1/Strings.java
@@ -0,0 +1,10 @@
+/*
+ * Written by Pranav Verma
+ * For the AP CSA Course.
+ */
+
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("String Literal.");
+    }
+}
