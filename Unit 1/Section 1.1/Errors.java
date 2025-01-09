e262f55514effcc22b14baebe73a6d52bbc3c2fd
Pranav Verma <pranav@verma.net.in>
2025-01-10 03:06:37 +0530

    Added Section 1.2

---
+++ Unit 1/Section 1.1/Errors.java
@@ -0,0 +1,12 @@
+/*
+ * Written by Pranav Verma
+ * For the AP CSA Course.
+ */
+
+public class Errors {
+    public static void main(String[] args) {
+        /* This Code will Generate Errors if Uncommented.
+        System.ou.println("Hello World!");
+        Systemout.println("Hello World!");*/
+    }
+}
---
+++ Unit 1/Section 1.2/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int DaysOfWeek;
+        final bool isRainy; // 'final' Keywords are Constant - Can't Be Changed After Dec.
+
+    }
+    
+}
