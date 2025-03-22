3ce6bab6f9213c6fba41b3d6189a7e2a9b712afa
Pranav Verma <pranav@verma.net.in>
2025-03-22 12:38:51 +0530

    Update: Added Section 3.3

---
+++ Unit 3/Section 3.3/if_else.java
@@ -0,0 +1,35 @@
+public class if_else {
+    public static void main(String[] args) {
+        /* if (boolean expression) {
+            Segment of code that will run if the boolean expression is true
+         }
+         else {
+            Segment of code that will run if the boolean expression is false
+         } */
+
+                // Create a bill for a (very expensive) birthday dinner
+                Bill birthdayDinner = new Bill(250.46, 14);
+                System.out.println("Birthday Dinner");
+                System.out.println(birthdayDinner);
+                
+                // Add the tip
+                birthdayDinner.addTip();
+                System.out.println("\nBirthday Dinner after tip");
+                System.out.println(birthdayDinner);
+                
+                
+                // Another dinner with less guests
+                System.out.println("\n\n"); // print a few blank lines
+                birthdayDinner = new Bill(250.46, 7);  // reassign the variable to a new object
+                System.out.println("Birthday Dinner");
+                System.out.println(birthdayDinner);
+                
+                // Add the tip
+                // Notice the difference in the final amount
+                birthdayDinner.addTip();
+                System.out.println("\nBirthday Dinner after tip");
+                System.out.println(birthdayDinner);
+                
+                
+    }
+}
