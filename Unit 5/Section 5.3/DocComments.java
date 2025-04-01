441e6dffb93105515c103bd152949bf541b58365
Pranav Verma <pranav@verma.net.in>
2025-04-01 15:15:23 +0530

    Update: Added Section 5.3

---
+++ Unit 5/Section 5.3/DocComments.java
@@ -0,0 +1,14 @@
+public class DocComments {
+    public static void main(String[] args) {
+        /* Comments
+         * - Ignored by compilers / interpreters
+         * - Help make code more readable
+         * - Prevent execution when testing alternative code
+         */
+
+        // These are Single-line Comments
+        /* These are Multi-line Comments */
+        
+    }
+}
+ 
