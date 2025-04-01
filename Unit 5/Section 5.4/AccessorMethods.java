dea9d075d687537705ee7e0f8f0cac09ea304527
Pranav Verma <pranav@verma.net.in>
2025-04-01 15:37:19 +0530

    Update: Added Section 5.4 & 5.5

---
+++ Unit 5/Section 5.4/AccessorMethods.java
@@ -0,0 +1,8 @@
+public class AccessorMethods {
+    public static void main(String[] args) {
+        Cow myCow = new Cow();
+
+        // getSound() is an accessor method.
+        System.out.println(myCow.getSound());
+    }
+}
---
+++ Unit 5/Section 5.5/MutatorMethods.java
@@ -0,0 +1,8 @@
+public class MutatorMethods {
+    public static void main(String[] args) {
+        Cow myCow = new Cow();
+
+        // milkCow() is an accessor method.
+        myCow.milkCow(10);
+    }
+}
