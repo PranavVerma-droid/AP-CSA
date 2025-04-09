65c260ef5e34c9eb43ea366f483ff3e3a79c8117
Pranav Verma <pranav@verma.net.in>
2025-04-09 13:15:27 +0530

    Update: Added Section 6.3

---
+++ Unit 6/Section 6.3/EnhancedLoops.java
@@ -0,0 +1,18 @@
+public class EnhancedLoops {
+    public static void main(String[] args) {
+        int [] values = {14, 523, 7685, -123, 370};
+
+        for (int number : values) {
+            System.out.println(number);
+        }
+
+        System.out.println("Finished!");;
+
+
+        String [] words = {"alpha", "beta", "gamma", "delta"};
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
