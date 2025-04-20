92a4edc2d277005e944dd741325d8dac1109c80b
Pranav Verma <pranav@verma.net.in>
2025-04-20 23:07:45 +0530

    Update: Added Section 7.4

---
+++ Unit 7/Section 7.4/AlgorithmsUsingArrayLists.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+@SuppressWarnings("unused")
+public class AlgorithmsUsingArrayLists {
+    public static void main(String[] args) {
+
+    }
+
+    
+    private double findMax(ArrayList<Double> values) {
+        double max = values.get(0);
+
+        for (int index = 1; index < values.size(); index++) {
+            if (values.get(index) > max) {
+                max = values.get(index);
+            }
+        }
+
+        return max;
+    }
+
+    private int findMin(ArrayList<Integer> values) {
+        int min = Integer.MAX_VALUE;
+
+        for (int currentValue : values) {
+            if (currentValue < min) {
+                min = currentValue;
+            }
+        }
+
+        return min;
+    }
+
+    private int findSum(ArrayList<Integer> values) {
+        int total = 0;
+        for (int currentValue : values) {
+            total = total + currentValue;
+        }
+
+        return total;
+    }
+}
